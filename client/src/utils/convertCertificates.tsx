b0953175c582a90f31cb8fde58eb45adfb5dfd4d
guyen Cong Hau (FIS DDCHCM) <haugc@fpt.com.vn>
2023-07-16 00:37:52 +0700

    build

--- client/src/utils/convertCertificates.tsx
+++
@@ -1,9 +0,0 @@
-import { type UserCertificate } from '@/models/UserCertificate'
-import _ from 'lodash'
-
-export const convertCertificates = (userCertificates: UserCertificate) => {
-  const data = _.map(userCertificates, (certificate) => {
-    return certificate.rmisCertificate
-  })
-  return data
-}
